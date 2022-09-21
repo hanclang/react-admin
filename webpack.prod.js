6704e019ea15b8e4ea7a45c21ad61a1eeecdfd07
hancl <874099925@qq.com>
2022-09-21 22:31:48 +0800

    变更action流程

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,4 +2,7 @@ const { merge } = require('webpack-merge')
 const baseConfig = require('./webpack.base.js')
 module.exports = merge(baseConfig, {
   mode: 'production',
+  output: {
+    publicPath: process.env.BUILD_ENV === 'github' ? 'react-admin' : '/',
+  },
 })
