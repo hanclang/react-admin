d9c9a31be84202709ee62242ccca62eae2561112
hancl <874099925@qq.com>
2022-09-21 22:43:34 +0800

    fix github 静态资源目录

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,6 @@ const baseConfig = require('./webpack.base.js')
 module.exports = merge(baseConfig, {
   mode: 'production',
   output: {
-    publicPath: process.env.BUILD_ENV === 'github' ? 'react-admin' : '/',
+    publicPath: process.env.BUILD_ENV === 'github' ? '/react-admin' : '/',
   },
 })
